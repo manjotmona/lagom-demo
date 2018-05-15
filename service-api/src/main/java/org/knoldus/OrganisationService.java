1da7549981fd39146e2ade6eb70293121722ed9c
Manjot Kaur <manjotmona@gmail.com>
2018-05-15 16:36:46 +0530

    Task-02 | Initial implementation for kafka

--- service-api/src/main/java/org/knoldus/OrganisationService.java
+++ service-api/src/main/java/org/knoldus/OrganisationService.java
@@ -4,17 +4,37 @@ import akka.NotUsed;
 import com.lightbend.lagom.javadsl.api.Descriptor;
 import com.lightbend.lagom.javadsl.api.Service;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
+import com.lightbend.lagom.javadsl.api.broker.Topic;
 import com.lightbend.lagom.javadsl.api.transport.Method;
 
 import static com.lightbend.lagom.javadsl.api.Service.restCall;
 import static com.lightbend.lagom.javadsl.api.Service.named;
+import static com.lightbend.lagom.javadsl.api.Service.pathCall;
+import static com.lightbend.lagom.javadsl.api.Service.topic;
 
 
 public interface OrganisationService extends Service {
+
+    //Topic name
+    String GREETINGS_TOPIC = "greeting";
+
+
     ServiceCall<NotUsed,String> helloMethodName();
+
+    ServiceCall<NotUsed, String> subscribe(String id);
+    ServiceCall<GreetingMessage,String> publish(String id);
 
     default Descriptor descriptor() {
         return named("service").withCalls(
-                restCall(Method.GET,"/balance",this::helloMethodName)).withAutoAcl(true);
+                restCall(Method.GET,"/balance",this::helloMethodName),
+                pathCall("/api/hello/:id", this::subscribe),
+                pathCall("/api/hello/:id", this::publish)
+        ).withTopics(
+                topic(GREETINGS_TOPIC, this::greetingsTopic)
+
+        ).withAutoAcl(true);
     }
+    Topic<GreetingMessage> greetingsTopic();
+
+
 }
--- service-impl/src/main/java/org/knoldus/OrganisationServiceImpl.java
+++ service-impl/src/main/java/org/knoldus/OrganisationServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.concurrent.CompletableFuture;
 
 import akka.NotUsed;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
+import com.lightbend.lagom.javadsl.api.broker.Topic;
 
 public class OrganisationServiceImpl implements OrganisationService{
     @Override public ServiceCall<NotUsed, String> helloMethodName() {
@@ -11,5 +12,17 @@ public class OrganisationServiceImpl implements OrganisationService{
         {
             return CompletableFuture.completedFuture("balance = 10");
         };
+    }
+
+    @Override public ServiceCall<NotUsed, String> subscribe(String id) {
+        return null;
+    }
+
+    @Override public ServiceCall<GreetingMessage, String> publish(String id) {
+        return null;
+    }
+
+    @Override public Topic<GreetingMessage> greetingsTopic() {
+        return null;
     }
 }
